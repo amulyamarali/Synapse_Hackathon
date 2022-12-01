d23d4ed27c2c8aab387a90b816feca30d9681ab4
Karthik <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2022-12-01 11:32:22 +0530

    web_app commit
    web_app commit

---
+++ synapse_web_application/src/footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+    <>
+        <div id = "footer">
+            <div id = "logos">
+                <ul>
+                <i className="fa-brands fa-instagram fa-3x iconclass insta"></i>
+                <i className="fa-brands fa-twitter fa-3x iconclass"></i>
+                <i className="fa-brands fa-github fa-3x iconclass git"></i>
+                </ul>
+            </div>
+
+        </div>
+    </>
+  )
+}
+
+export default Footer
